a22c16a0b6093d09b2a5ba526043f490605ed436
Mariusz Bielesz <bielesz@yahoo.com>
2023-02-18 18:15:44 +0100

    WP to staic page

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-900-1676740182.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'DOMContentLoaded', function() {
+	UAGBInlineNotice.init( {"c_id":null,"cookies":false,"close_cookie_days":1,"noticeDismiss":"","icon":"rectangle-xmark"}, '.uagb-block-a5e9cab1' );
+});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-4acac95d' );
+	});
+ })
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-904-1676740351.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){ var ssLinks = document.querySelectorAll( '.uagb-block-d9392ee0' );
+for ( var j = 0; j < ssLinks.length; j++ ) {
+	var ssLink = ssLinks[j].querySelectorAll( ".uagb-ss__link" );
+	for ( var i = 0; i < ssLink.length; i++ ) {
+		ssLink[i].addEventListener( "click", function() {
+			var social_url = this.dataset.href;
+			var target = "";
+			if( social_url == "mailto:?body=" ) {
+				target = "_self";
+			}
+			var  request_url ="";
+			if( social_url.indexOf("/pin/create/link/?url=") !== -1) {
+				request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
+			}else{
+				request_url = social_url + encodeURIComponent( window.location.href );
+			}
+			window.open( request_url, target );
+		});
+	}
+}
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-4acac95d' );
+	});
+ })
